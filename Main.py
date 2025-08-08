2a5b6dee80243d64cd9139ed52780976e4a07e97
AstridIsVeryCool <146063339+AstridIsVeryCool@users.noreply.github.com>
2025-08-07 19:50:57 -0700

    added function to remove codes reducible by rule 5, also added todo list

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import copy
+
+#generates a list of all ways to order numbers up to max_value with number_of_elements slots
 def order_values(max_value, number_of_elements):
     working_list = [0] * number_of_elements
     working_value_matrix = []
@@ -21,6 +23,9 @@ def order_values(max_value, number_of_elements):
             done_flag = True
     return working_value_matrix
 
+
+#removes cofrd that are mirrored versions of another code (leaving one of the two mirrored versions ofc)
+#WARNING!!!!! ONLY WORKS WITH CODES WITH ONE EDGE AS OF 7/7/25
 def remove_duplicates(matrix):
     seen = set()
     unique = []
@@ -35,7 +40,9 @@ def remove_duplicates(matrix):
     return unique
 
 
-
+#generates all codes that fit a certain template
+#WARNING!!!!!!! ONLY WORKS WITH CODES WITH ONE EDGE AS OF 7/7/25
+#To do: generalize to other code types
 def generate_codes_within_family(matrix, template):
     ribbon_codes = []
     for i in matrix:
@@ -45,13 +52,61 @@ def generate_codes_within_family(matrix, template):
     print(len(ribbon_codes))
     return ribbon_codes
 
+def get_valence():
+    pass
+    #to do
+
+#applies rule 5( and also 6) from Bounds, Jones determinants, irreducible codes to a list of ribbon codes to remove reducible codes
+def apply_rule_5(code_list):
+    filtered_code_list = []
+
+    for code in code_list:
+        should_remove = False
+        for column in code:
+            for marking in column:
+                if marking == 0:
+                    continue
+                if not isinstance(marking, list):
+                    continue
+                if len(marking) < 2:
+                    continue
+
+                last_val = marking[0]
+                for val in marking[1:]:
+                    if val == last_val:
+                        should_remove = True
+                        break
+                    last_val = val
+
+                if should_remove:
+                    break
+            if should_remove:
+                break
+
+        if not should_remove:
+            filtered_code_list.append(code)
+
+    return filtered_code_list
+
+                        
+                    
 
 
+#applies reduction rules to remove reducible codes
+#Not finished as of 7/7/35
+def remove_reducible_codes(code_list):
+    working_list = apply_rule_5(code_list)
     
 four_vertex_template = [[[],0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 x = order_values(3, 4)
 x = remove_duplicates(x)
 
-print(generate_codes_within_family(x,four_vertex_template))
+code_list = generate_codes_within_family(x,four_vertex_template)
+
+print(code_list)
+for i in range(10):
+    print()
+
+print(apply_rule_5(code_list))
 
